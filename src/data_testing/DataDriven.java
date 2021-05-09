4428d20b5274b89cbec86e925fff490fd96ed109
doTruongGiang-IT <dotruonggiang290@gmail.com>
2021-05-09 16:47:20 +0700

    Add data driven testing

--- src/data_testing/DataDriven.java
+++ src/data_testing/DataDriven.java
@@ -1,18 +1,77 @@
 package data_testing;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import base_class.BaseClass;
 import page_factory.PageFac;
 
 public class DataDriven extends BaseClass {
 
-	@Test(priority = 1, description = "This TC will get data from database and export excel file")
-	public void getData() {
-		PageFac getData = PageFactory.initElements(driver, PageFac.class);
-		getData.exportData();
+	@Test(priority = 1, description="This TC will get account data and export do excel")
+	public void getAccountData() {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		PageFac getAccountData = PageFactory.initElements(driver, PageFac.class);
+		getAccountData.exportAccountData();
 	};
 	
+	@Test(priority = 2, description="This TC will get book data and export do excel")
+	public void getBookData() {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		PageFac getBookData = PageFactory.initElements(driver, PageFac.class);
+		getBookData.exportBookData();
+	};
 	
+	@Test(priority = 3, dataProvider = "bookdata", description = "This TC will check book data")
+	public void searchBook(String name) throws Exception {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		PageFac searchBook = PageFactory.initElements(driver, PageFac.class);
+		if(name != null) {
+			searchBook.searchBook(name, driver);
+		};
+	};
+	
+	@DataProvider(name = "bookdata", parallel = false)
+	public Object[] TestDataFeed() throws Exception{
+		
+		Object[] book = new Object[16];
+		
+		File src=new File("C:\\Users\\DELL\\eclipse-workspace\\Do_An_KTPM\\excelData\\BookData.xlsx");
+		FileInputStream fis = new FileInputStream(src);
+		 
+		// load the workbook
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		 
+		// get the sheet which you want to modify or create
+		XSSFSheet sheet1 = workbook.getSheetAt(0);
+		
+		//Iterate through each row from first sheet
+		Iterator<Row> rowIterator = sheet1.iterator();
+		 
+		while (rowIterator.hasNext()) {
+			  Row row = rowIterator.next();
+	
+			  //For each row iterate through each columns
+			  Iterator <Cell> cellIterator = row.cellIterator();
+	
+			  while (cellIterator.hasNext()) {
+				  Cell cell = cellIterator.next(); 
+				  if(cell.getColumnIndex() == 0 && row.getRowNum() >= 1) {
+					  book[row.getRowNum()] = cell.getStringCellValue();  
+				  };
+			  };
+		};
+		
+		workbook.close();
+		return book;
+	};
 	
 }
--- src/page_factory/PageFac.java
+++ src/page_factory/PageFac.java
@@ -11,15 +11,34 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PageFac {
 	
-	public void exportData() {
+	@FindBy(how = How.XPATH, using = "//*[@id=\"wn__header\"]/div/div[1]/div[3]/ul/li[1]/a")
+	@CacheLookup
+	WebElement icon_search;
+	
+	@FindBy(how = How.XPATH, using = "//*[@id=\"search_mini_form\"]/div/input[1]")
+	@CacheLookup
+	WebElement searchInput;
+	
+	@FindBy(how = How.XPATH, using = "//*[@id=\"search_mini_form\"]/div/div/button")
+	@CacheLookup
+	WebElement search_button;
+	
+	public void exportAccountData() {
 		String jdbcURL = "jdbc:mysql://localhost:3306/webdb?useSSL=false";
         String username = "root";
         String password = "";
  
-        String excelFilePath = "C:\\Users\\DELL\\eclipse-workspace\\Do_An_KTPM\\excelData\\DataDriven.xlsx";
+        String excelFilePath = "C:\\Users\\DELL\\eclipse-workspace\\Do_An_KTPM\\excelData\\Account.xlsx";
  
         try (Connection connection = DriverManager.getConnection(jdbcURL, username, password)) {
             String sql = "SELECT * FROM tbltaikhoan";
@@ -29,7 +48,7 @@ public class PageFac {
             ResultSet result = statement.executeQuery(sql);
  
             XSSFWorkbook workbook = new XSSFWorkbook();
-            XSSFSheet sheet = workbook.createSheet("Data");
+            XSSFSheet sheet = workbook.createSheet("AccountData");
  
             Row headerRow = sheet.createRow(0);
             
@@ -75,6 +94,70 @@ public class PageFac {
             System.out.println("File IO error:");
             e.printStackTrace();
         };
+	};
+	
+	public void exportBookData() {
+		String jdbcURL = "jdbc:mysql://localhost:3306/webdb?useSSL=false";
+        String username = "root";
+        String password = "";
+ 
+        String excelFilePath = "C:\\Users\\DELL\\eclipse-workspace\\Do_An_KTPM\\excelData\\BookData.xlsx";
+ 
+        try (Connection connection = DriverManager.getConnection(jdbcURL, username, password)) {
+            String sql = "SELECT * FROM tblsach";
+ 
+            Statement statement = connection.createStatement();
+ 
+            ResultSet result = statement.executeQuery(sql);
+ 
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            XSSFSheet sheet = workbook.createSheet("BookData");
+ 
+            Row headerRow = sheet.createRow(0);
+            
+            Cell headerCell = headerRow.createCell(0);
+            headerCell.setCellValue("Ten Sach");
+     
+            headerCell = headerRow.createCell(1);
+            headerCell.setCellValue("Gia Ban");
+ 
+            int rowCount = 1;
+            
+            while (result.next()) {
+                String name = result.getString("tenSach");
+                String price = result.getString("GiaBan");
+     
+                Row row = sheet.createRow(rowCount++);
+     
+                int columnCount = 0;
+                Cell cell = row.createCell(columnCount++);
+                cell.setCellValue(name);
+     
+                cell = row.createCell(columnCount++);
+                cell.setCellValue(price);
+            };
+ 
+            FileOutputStream outputStream = new FileOutputStream(excelFilePath);
+            workbook.write(outputStream);
+            workbook.close();
+ 
+            statement.close();
+ 
+        } catch (SQLException e) {
+            System.out.println("Datababse error:");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("File IO error:");
+            e.printStackTrace();
+        };
+	};
+	
+	public void searchBook(String name, WebDriver driver) throws Exception {
+		icon_search.click();
+		Thread.sleep(2000);
+		searchInput.sendKeys(name);
+		WebDriverWait wait = new WebDriverWait(driver, 500);
+		wait.until(ExpectedConditions.elementToBeClickable(search_button)).click();
 	};
 
 }
